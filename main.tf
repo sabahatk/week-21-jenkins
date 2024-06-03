c5d90d89ba7af658fe52ae7f750dcb526d1ffe47
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2024-06-03 12:02:07 -0400

    Updated main.tf file to include variables instead

--- main.tf
+++ main.tf
@@ -1,67 +1,59 @@
-#Set providers to AWS
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region_name
 }
 
 #Add security group to allow port 22 and 8080 traffic
 resource "aws_security_group" "TF_SG" {
-  name        = "security group using Terraform"
-  description = "security group using Terraform"
-  vpc_id      = "vpc-00bdbfa80f37bd0fe"
+  name        = var.sg_name
+  description = var.sg_name
+  vpc_id      = var.vpc_id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = var.ssh_desc
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = var.protocol_tcp
+    cidr_blocks = var.cidr_block
   }
 
   ingress {
-    description = "Jenkins"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = var.jenkins_desc
+    from_port   = var.jenkins_port
+    to_port     = var.jenkins_port
+    protocol    = var.protocol_tcp
+    cidr_blocks = var.cidr_block
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.outbound_port
+    to_port     = var.outbound_port
+    protocol    = var.protocol_outbound
+    cidr_blocks = var.cidr_block
   }
 
   tags = {
-    Name = "TF_SG"
+    Name = var.sg_tag
   }
 }
 
 #create s3 bucket
 resource "aws_s3_bucket" "TF_Bucket" {
-  bucket = "my-tf-jenkins-bucket-may-2024-i382k4m2l"
-  
+  bucket = var.bucket_name
+
   tags = {
-    Name = "Jenkins Bucket"
+    Name = var.bucket_tag
   }
 }
 
 #Create instance
 resource "aws_instance" "jenkins" {
-  ami             = "ami-00beae93a2d981137"
-  instance_type   = "t2.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type_name
   vpc_security_group_ids = [aws_security_group.TF_SG.id]
 
   tags = {
-    Name = "JenkinsInstance"
+    Name = var.instance_name
   }
-  user_data = file("~/environment/terraform-project-week21/user-data.sh")
+  user_data = file(var.ud_filepath)
 }
